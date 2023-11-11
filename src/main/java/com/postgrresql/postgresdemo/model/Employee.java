744068fae88ffc9b8eeb78418d24ff129e053f2a
antonio <ac.pollazzo@gmail.com>
2023-11-11 16:04:15 +0000

    Initial commit

---
+++ src/main/java/com/postgrresql/postgresdemo/model/Employee.java
@@ -0,0 +1,46 @@
+package com.postgrresql.postgresdemo.model;
+
+import jakarta.persistence.*;
+
+import java.time.LocalDate;
+
+@Entity
+@Table(name = "employees")
+public class Employee {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Integer id;
+
+    private String firstName;
+
+    private String lastName;
+
+    private LocalDate dateOfBirth;
+
+    // Hibernate expects entities to have a no-arg constructor,
+    // though it does not necessarily have to be public.
+    private Employee() {}
+
+    public Employee(String firstName, String lastName, LocalDate dateOfBirth) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.dateOfBirth = dateOfBirth;
+    }
+
+    public Integer getId() {
+        return this.id;
+    }
+
+    public String getFirstName() {
+        return this.firstName;
+    }
+
+    public String getLastName() {
+        return this.lastName;
+    }
+
+    public LocalDate getDateOfBirth() {
+        return this.dateOfBirth;
+    }
+}
